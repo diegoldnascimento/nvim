bdb3f1aa005f2fb3b0606e14817e35e7da06ac82
Diego <diego@pop-os.localdomain>
2023-08-04 18:00:28 -0300

    fix(plugins): fixing the bufferline command to kill buffers using bufferline

--- lua/core/plugins/bufferline.lua
+++ lua/core/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
 	"akinsho/bufferline.nvim",
-  event = "VeryLazy",
+	event = "VeryLazy",
 	config = function()
 		local status_ok, bufferline = pcall(require, "bufferline")
 		if not status_ok then
@@ -9,29 +9,34 @@ return {
 
 		bufferline.setup({
 			options = {
-				numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-				close_command = "bdelete! %d",
-				right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-				left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-				middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+				mode = "buffers",
+				numbers = "none",
+				close_command = function(bufnr)
+					require("bufdel").delete_buffer_expr(bufnr)
+				end,
+				right_mouse_command = "vert sbuffer %d",
+				left_mouse_command = "buffer %d",
+				middle_mouse_command = nil,
 				buffer_close_icon = "",
 				modified_icon = "●",
 				close_icon = "",
 				left_trunc_marker = "",
 				right_trunc_marker = "",
 				max_name_length = 30,
-				max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+				max_prefix_length = 30,
 				tab_size = 21,
-				diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+				diagnostics = "nvim_lsp",
 				diagnostics_update_in_insert = false,
+				color_icons = true,
 				show_buffer_icons = true,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				show_tab_indicators = true,
-				persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-				separator_style = "thick", -- | "thick" | "thin" | { 'any', 'any' },
+				persist_buffer_sort = true,
+				separator_style = "thin",
 				enforce_regular_tabs = true,
 				always_show_bufferline = true,
+				sort_bv = "id",
 				offsets = {
 					{ filetype = "NvimTree" },
 				},
