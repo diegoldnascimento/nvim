c370a3c7aad6e36dfef886ebb2850a11f20dd23b
Diego <diego@pop-os.localdomain>
2023-12-27 14:05:57 -0300

    feat: replacing the dracula colors by rose pine colors

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -50,5 +50,5 @@ return {
 	-- 	end,
 	-- }{
 	{ "Mofiqul/dracula.nvim", lazy = false, priority = 1000 },
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'rose-pine/neovim', name = 'rose-pine' },
 }
