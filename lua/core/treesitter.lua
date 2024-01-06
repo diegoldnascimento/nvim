5a007f81542335f483e5d50c4ac168e7993b23a8
Diego <diego@pop-os.localdomain>
2024-01-05 21:16:26 -0300

    feat: remove the rainbow delimiters

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -60,24 +60,3 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
-local rainbow_delimiters = require("rainbow-delimiters")
-
-vim.g.rainbow_delimiters = {
-	strategy = {
-		[""] = rainbow_delimiters.strategy["global"],
-		vim = rainbow_delimiters.strategy["local"],
-	},
-	query = {
-		[""] = "rainbow-delimiters",
-		lua = "rainbow-blocks",
-	},
-	highlight = {
-		"RainbowDelimiterViolet",
-		"RainbowDelimiterYellow",
-		"RainbowDelimiterCyan",
-		"RainbowDelimiterBlue",
-		"RainbowDelimiterOrange",
-		"RainbowDelimiterGreen",
-		"RainbowDelimiterRed",
-	},
-}
