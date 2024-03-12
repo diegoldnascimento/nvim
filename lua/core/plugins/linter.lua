582ebde531d48b184e567e702f7c9552d6f965b4
Diego <diego@pop-os.localdomain>
2024-03-12 17:35:50 -0300

    feat(nvim-lint): adding the right config for nvim-lint

--- lua/core/plugins/linter.lua
+++ lua/core/plugins/linter.lua
@@ -1,21 +1,32 @@
 return {
 	{
 		"mfussenegger/nvim-lint",
-		event = "VeryLazy",
-		config = function()
-			require("lint").linters_by_ft = {
-				typescript = { "eslint" },
-				javascript = { "eslint" },
+		events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		opts = {
+			linters_by_ft = {
+				typescript = { "eslint_d" },
+				javascript = { "eslint_d" },
 				json = { "jsonlint" },
 				python = { "flake8" },
 				terraform = { "tflint" },
-			}
+			},
 
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
+			linters = {
+				eslint_d = {
+					args = {
+						"--no-warn-ignored",
+						"--format",
+						"json",
+						"--stdin",
+						"--stdin-filename",
+						function()
+							return vim.api.nvim_buf_get_name(0)
+						end,
+					},
+				},
+			},
+		},
+		config = function()
 		end,
 	},
 }
