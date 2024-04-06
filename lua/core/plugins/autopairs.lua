8c8bce04a0a55d543749d90520c1242659c2989f
Diego Lopes <503598-diegolopes@users.noreply.gitlab.com>
2024-04-06 17:10:23 -0300

    feat(plugins/autopairs): remove support to fast_wrap key

--- lua/core/plugins/autopairs.lua
+++ lua/core/plugins/autopairs.lua
@@ -5,17 +5,6 @@ return {
 		opts = {
 			check_ts = true,
 			ts_config = { java = false },
-			fast_wrap = {
-				map = "<M-e>",
-				chars = { "{", "[", "(", '"', "'" },
-				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-				offset = 0,
-				end_key = "$",
-				keys = "qwertyuiopzxcvbnmasdfghjkl",
-				check_comma = true,
-				highlight = "PmenuSel",
-				highlight_grey = "LineNr",
-			},
 		},
 		config = function(_, opts)
 			local npairs = require("nvim-autopairs")
