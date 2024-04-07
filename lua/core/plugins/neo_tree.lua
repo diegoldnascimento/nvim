91afc0a82e5690123256241cdba5d4da38d18507
Diego <diego@pop-os.localdomain>
2024-04-07 17:23:36 -0300

    show hidden files

--- lua/core/plugins/neo_tree.lua
+++ lua/core/plugins/neo_tree.lua
@@ -154,11 +154,12 @@ return {
 			bind_to_cwd = false,
 			filtered_items = {
 				visible = false, -- when true, they will just be displayed differently than normal items
-				hide_dotfiles = true,
+				hide_dotfiles = false,
 				hide_gitignored = true,
 				hide_hidden = false, -- only works on Windows for hidden files/directories
 				hide_by_name = {
 					"node_modules",
+					".git",
 				},
 				always_show = { -- remains visible even if other settings would normally hide it
 					".gitignored",
