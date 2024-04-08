ce08124349cd6c9e4743c593ad183e2fd534c0a5
Diego <diego@pop-os.localdomain>
2024-04-08 12:21:26 -0300

    fix: rest plugins

--- lua/core/plugins/rest.lua
+++ lua/core/plugins/rest.lua
@@ -1,71 +1,37 @@
+-- Lazy loading LuaRocks dependencies for lazy.nvim
 return {
 	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
 		"rest-nvim/rest.nvim",
-		dependencies = { {
-			"vhyrro/luarocks.nvim",
-			priority = 1000,
-			config = true,
-		} },
 		ft = "http",
+		dependencies = { "luarocks.nvim" },
+		build = function()
+			local ok, luarocks = pcall(require, "luarocks.rocks")
+
+			luarocks.ensure({
+				"nvim-nio ~> 1.7",
+				"lua-curl ~> 0.3",
+				"mimetypes ~> 1.0",
+				"xml2lua ~> 1.5",
+			})
+		end,
 		keys = {
 			{
 				"<leader>rr",
-				"<cmd>Rest run<cr>",
-				mode = { "n" },
+				"<cmd>Rest run<CR>",
+				mode = { "n", "v" },
 				desc = "Run request under the cursor",
 			},
 			{
 				"<leader>rl",
-				"<cmd>Rest run last<cr>",
-				mode = { "n" },
-				desc = "Run request under the cursor",
+				"<cmd>Rest run last<CR>",
+				mode = { "n", "v" },
+				desc = "Re-run latest request",
 			},
-		},
-		opts = {
-			-- Open request results in a horizontal split
-			result_split_horizontal = false,
-			-- Keep the http file buffer above|left when split horizontal|vertical
-			result_split_in_place = false,
-			-- stay in current windows (.http file) or change to results window (default)
-			stay_in_current_window_after_split = false,
-			-- Skip SSL verification, useful for unknown certificates
-			skip_ssl_verification = false,
-			-- Encode URL before making request
-			encode_url = true,
-			-- Highlight request on run
-			highlight = {
-				enabled = true,
-				timeout = 150,
-			},
-			result = {
-				-- toggle showing URL, HTTP info, headers at top the of result window
-				show_url = true,
-				-- show the generated curl command in case you want to launch
-				-- the same request via the terminal (can be verbose)
-				show_curl_command = false,
-				show_http_info = true,
-				show_headers = true,
-				-- table of curl `--write-out` variables or false if disabled
-				-- for more granular control see Statistics Spec
-				show_statistics = false,
-				-- executables or functions for formatting response body [optional]
-				-- set them to false if you want to disable them
-				formatters = {
-					json = "jq",
-					html = function(body)
-						return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-					end,
-				},
-			},
-			-- Jump to request line on run
-			jump_to_request = false,
-			env_file = ".env",
-			-- for telescope select
-			env_pattern = "\\.env$",
-			env_edit_command = "tabedit",
-			custom_dynamic_variables = {},
-			yank_dry_run = true,
-			search_back = true,
 		},
 		config = function(_, opts)
 			require("rest-nvim").setup(opts)
