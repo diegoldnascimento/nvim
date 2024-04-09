3fddd8a3af4ad86b3f19674816b9725078f84c92
Diego <diego@pop-os.localdomain>
2024-04-09 12:42:05 -0300

    feat(plugins/colorizer): remove the event verylazy to make this work properly

--- lua/core/plugins/colorizer.lua
+++ lua/core/plugins/colorizer.lua
@@ -1,21 +1,18 @@
 return {
 	"nvchad/nvim-colorizer.lua",
-	event = "VeryLazy",
 	opts = {
 		filetypes = { "*" },
 		user_default_options = {
-			RGB = true, -- #RGB hex codes
-			RRGGBB = true, -- #RRGGBB hex codes
-			names = true, -- "Name" codes like Blue or blue
-			RRGGBBAA = false, -- #RRGGBBAA hex codes
-			AARRGGBB = false, -- 0xAARRGGBB hex codes
-			rgb_fn = false, -- CSS rgb() and rgba() functions
-			hsl_fn = false, -- CSS hsl() and hsla() functions
-			css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-			css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-			mode = "background", -- Set the display mode.
-			tailwind = false, -- Enable tailwind colors
-			sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+			RGB = true,
+			RRGGBB = true,
+			names = true,
+			RRGGBBAA = false,
+			AARRGGBB = false,
+			rgb_fn = false,
+			hsl_fn = false,
+			css = false,
+			css_fn = false,
+			mode = "background",
 			virtualtext = "■",
 			always_update = false,
 		},
