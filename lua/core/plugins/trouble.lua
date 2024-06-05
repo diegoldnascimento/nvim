2eda6eea20ec74df2ba583fb20ab90f5d8e33995
Diego Lopes <503598-diegolopes@users.noreply.gitlab.com>
2024-06-05 13:19:19 -0300

    fix(plugins/trouble): fix the keymappings and commands for Trouble plugin. It was wrong

--- lua/core/plugins/trouble.lua
+++ lua/core/plugins/trouble.lua
@@ -1,21 +1,18 @@
 return {
 	"folke/trouble.nvim",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 	config = function()
-		require("trouble").setup({})
-		local keymap = vim.keymap
-
-		keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>")
-		keymap.set("n", "<leader>xw", function()
-			require("trouble").open("workspace_diagnostics")
-		end)
-		keymap.set("n", "<leader>xd", function()
-			require("trouble").open("document_diagnostics")
-		end)
-		keymap.set("n", "<leader>xq", function()
-			require("trouble").open("quickfix")
-		end)
-		keymap.set("n", "<leader>xl", function()
-			require("trouble").open("loclist")
-		end)
+		require("trouble").setup()
 	end,
 }
