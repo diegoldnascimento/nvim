315f6b2fdcff18c38a9c3095dac9f26f1c46900a
Diego <diego.lopes.2402@gmail.com>
2025-04-03 01:14:02 -0300

    feat: add trouble support on lualine

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -30,8 +30,8 @@ return {
 					c = { fg = colors.black, bg = colors.black },
 				},
 			}
-			--
-			require("lualine").setup({
+
+			local opts = {
 				options = {
 					theme = dracula,
 					globalstatus = true,
@@ -58,7 +58,25 @@ return {
 						"filetype",
 					},
 				},
+			}
+
+			local trouble = require("trouble")
+			local symbols = trouble.statusline({
+				mode = "lsp_document_symbols",
+				groups = {},
+				title = false,
+				filter = { range = true },
+				format = "{kind_icon}{symbol.name:Normal}",
+				-- The following line is needed to fix the background color
+				-- Set it to the lualine section you want to use
+				hl_group = "lualine_c_normal",
 			})
+			table.insert(opts.sections.lualine_c, {
+				symbols.get,
+				cond = symbols.has,
+			})
+			--
+			require("lualine").setup(opts)
 		end,
 	},
 }
