f410bfaf306fcaf3c5321b828491004ae3546e76
Diego <diego.lopes.2402@gmail.com>
2025-04-27 03:00:06 -0300

    feat: use dracula-soft over dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,15 +14,26 @@ return {
 					visual = "#3E4452",
 					gutter_fg = "#4B5263",
 					nontext = "#3B4048",
+					selection = "#44475A",
+					bright_white = "#FFFFFF",
+					white = "#ABB2BF",
+					black = "#191A21",
 				},
-				show_end_of_buffer = false,
+				show_end_of_buffer = true,
 				transparent_bg = true,
 				italic_comment = true,
 				overrides = {},
 			})
 
-			vim.cmd([[colorscheme dracula]])
+			vim.cmd([[colorscheme dracula-soft]])
 		end,
 	},
+	-- {
+	-- 	"forest-nvim/sequoia.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme sequoia-night") -- or 'sequoia-night' / 'sequoia-rise'
+	-- 	end,
+	-- },
 }
-
