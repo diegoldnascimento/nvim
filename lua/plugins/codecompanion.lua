c7b267e025d82ed7e4d51e07124cf7461485a419
Diego <diego.lopes.2402@gmail.com>
2025-09-12 11:15:29 -0300

    feat(markview): add the markview plugin

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		opts = {
+			preview = {
+				filetypes = { "markdown", "codecompanion" },
+				ignore_buftypes = {},
+			},
+		},
+	},
+	{
 		"olimorris/codecompanion.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
